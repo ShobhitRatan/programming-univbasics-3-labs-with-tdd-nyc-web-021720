0e7e761fa5a5436c7094ae1a30e5504813c26eee
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-01-29 05:54:11 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,29 @@
 # Add your variables here
+# Value for the 1st number. 
+def first_number(value)
+  puts value
+end 
+
+first_number(2)
+
+# Value for the second number
+def second_number(value)
+  puts value
+end 
+
+second_number(4)
+
+# Sum of two values. 
+def sum(first_number, second_number)
+  value = first_number + second_number 
+  puts value 
+end 
+
+sum(2,4)
+
+# Difference of two values. 
+def difference(first_number, second_number)
+  value = second_number - first_number 
+  puts value 
+end 
+
