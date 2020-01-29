a86bae1a6fec63685e5d11d1e364f9009cf2daf7
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-01-29 06:03:06 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 # Value for the 1st number.
-first_number = 'Hello_world'
+first_number = 2
 second_number = 4
 sum = first_number + second_number
 difference = second_number - first_number
